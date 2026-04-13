0e71cbad7ad64400fc040c241eae48a8a28d3974
jihanhikmalani <jhl503035@gmail.com>
2026-04-13 10:50:06 +0700

    update

--- main.js
+++ main.js
@@ -8,7 +8,10 @@ import {
   deleteDoc,
   updateDoc,
   doc,
-  getDoc
+  getDoc,
+  query,
+  where
+  
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
 // GANTI DENGAN FIREBASE CONFIG ANDA
@@ -24,6 +27,36 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 const tanamanCollection = collection(db, "tanaman")
+
+export async function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+const q = query(
+  collection(db, "users"),
+  where("username", "==", username),
+  where("password", "==", password)
+)
+
+  const querySnapshot = await getDocs(collection(db, "users"));
+
+  let found = false;
+
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+
+    if (data.username === username && data.password === password) {
+      found = true;
+    }
+  });
+
+  if (found) {
+    document.getElementById("status").innerText = "Login berhasil";
+    // redirect
+    window.location.href = "index.html";
+  } else {
+    document.getElementById("status").innerText = "Username atau password salah";
+}
 
 // fungsi untuk menampilkan daftar tanaman
 export async function tampilkanDaftarTanaman() {
@@ -137,4 +170,5 @@ export async function ubahDataTanaman(id, namaTanaman, warna, jenis) {
   
   // alihkan ke halaman daftar siswa 
   window. location. href = 'daftar.html'
+}
 }
